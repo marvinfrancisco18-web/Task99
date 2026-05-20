2f93dd2f623fa63828f89c1d14fffb7f7c365e8c
marvinfrancisco18-web <marvinfrancisco18@gmail.com>
2026-05-20 11:50:19 +0800

    Update Task2.java

--- Task2.java
+++ Task2.java
@@ -2,15 +2,15 @@ public class Task2 {
     public static void main(String[] args) {
 
         // primitive variables
-        char letter1 = 'H';
-        byte three = 3;
+        char letter1 = 'V';
+        byte two = 2;
         short one = 1;
         int zero = 0;
         float version = 2.0f;
         boolean value = true;
 
         // concatenate into one string
-        String output = letter1 + "" + three + one + one + zero
+        String output = letter1 + "" + two + one + one + zero
                 + " w" + zero + "rld "
                 + version + " " + value;
 
